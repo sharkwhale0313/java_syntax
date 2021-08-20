d92bfa2abc16c868a64903ded966949df5d56311
00030 <jowl950313@gmail.com>
2021-08-21 07:24:30 +0900

    intellij

---
+++ src/Datatype.java
@@ -0,0 +1,16 @@
+public class Datatype {
+    public static void main(String[] args){
+        // 정수
+        int fifteen = 15;           // 10진수
+        byte fifteen2 = 0b1111;     // 2진수 15
+        short fifteen3 = 017;       // 8진수 15
+        int fifteen4 = 0xF;         // 16진수 15
+        long lightSpeed = 300000L;  // L로 long type 임을 명시
+        // 실수
+        double half = 0.5;          // 일반 표기법
+        double half2 = 5E-1;        // 지수 표기법으로 5x10^-1
+        //float pi = 3.14159;       // 오류
+        float pi = 3.141592F;       // F로 float type 임을 명시
+        double pi2 = 3.14159;
+    }
+}
