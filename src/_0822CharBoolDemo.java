8f215885d1de5da6f1a670a32cb3e0652ce44927
00030 <jowl950313@gmail.com>
2021-08-22 07:52:48 +0900

    intellij

---
+++ src/_0822CharBoolDemo.java
@@ -0,0 +1,15 @@
+public class _0822CharBoolDemo {
+    public static void main(String[] args){
+        // char
+        char ga1 = '가';
+        char ga2 = '\uac00'; // 유니코드 가
+
+        // bool
+        boolean cham = true;
+        boolean geojit = false;
+
+        System.out.println(ga1);
+        System.out.println(ga2);
+        System.out.println(cham + "는 참이고 " + geojit + "는 거짓입니다. ");
+    }
+}
---
+++ src/_0822Constant.java
@@ -0,0 +1,28 @@
+public class _0822Constant {
+    public static void main(String[] args){
+        // 상수 : 프로그램 실행 중 변경할 수 없는 데이터
+        // EX] pi = 3.141592 or mach = 3*10^8 m/s 등
+        // 상수 이름은 일반 변수와 구분하기 위해 모두 '대문자'로 표기
+        // key word는 'final'
+        final double PI = 3.14159;
+
+        // 자동 타입 변환
+        double d1 = 5 * 3.14; // 정수 5를 5.0으로 자동 타입변환
+        double d2 = 1;        // 정수 1을 1.0으로 자동 타입변환
+
+        // 강제 타입 변환
+
+        // double인 3.14를 float으로 형변환 하여 f 에 3.14F 저장
+        float f = (float)3.14;
+
+        // int인 300을 byte로 형변환하면 -> '데이터 손실 발생'
+        // byte = 8 bit = 2^8 = -128 ~ 127
+        byte b = (byte)300;
+
+        // double인 3.14를 byte로 형변환하면 데이터가 손실되고 3만 저장
+        byte x = (byte)3.14;
+
+        // float인 3.14를 double로 형변환하면 데이터 손실 없이 저장
+        double d = (double)3.14f;
+    }
+}
