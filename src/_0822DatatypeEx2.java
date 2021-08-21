c228e5847e6ff0d70510c8c641bcf33a5057ab84
00030 <jowl950313@gmail.com>
2021-08-22 07:38:50 +0900

    intellij

---
+++ src/_0822DatatypeEx2.java
@@ -0,0 +1,14 @@
+public class _0822DatatypeEx2 {
+    public static void main(String[] args){
+        // character
+        char c = 'A';
+        char c2 = 65;
+        char c3 = '\u0041';
+        //char c4 = "A"; // 오류 : A가 string 이기 때문에 char에는 넣을 수 없음
+
+        // boolean
+        boolean condition = true;
+        boolean condition2 = false;
+
+    }
+}
