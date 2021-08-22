c805ef68d4c6a5ce1c42b87bbd5bfb20e23d014a
00030 <jowl950313@gmail.com>
2021-08-23 03:09:40 +0900

    print문의 종류

---
+++ src/_0823_Print.java
@@ -0,0 +1,12 @@
+public class _0823_Print {
+    public static void main(String[] args){
+        // println() : 출력 후 줄바꿈
+        // print()   : 단순 출력 (줄바꿈 x)
+        // printf()  :  포맷 지정 출력
+
+        int x = 5;
+        double pi = 3.14;
+        System.out.printf("x = %d and pi = %f\n", x, pi);
+        // %d, %f : 포맷 명시자
+    }
+}
