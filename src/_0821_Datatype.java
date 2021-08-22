37d946a5f285f30f783d687d9a870397120652f5
00030 <jowl950313@gmail.com>
2021-08-23 02:56:29 +0900

    여러가지 데이터 타입

--- src/_0821_Datatype.java
+++ src/_0821_Datatype.java
@@ -6,6 +6,7 @@ public class _0821_Datatype {
         short fifteen3 = 017;       // 8진수 15
         int fifteen4 = 0xF;         // 16진수 15
         long lightSpeed = 300000L;  // L로 long type 임을 명시
+
         // 실수
         double half = 0.5;          // 일반 표기법
         double half2 = 5E-1;        // 지수 표기법으로 5x10^-1
