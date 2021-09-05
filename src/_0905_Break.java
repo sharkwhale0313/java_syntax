e88e73606dbe6080c7979ba869975c2683735097
00030 <jowl950313@gmail.com>
2021-09-05 15:52:55 +0900

    break문

---
+++ src/_0905_Break.java
@@ -0,0 +1,10 @@
+public class _0905_Break {
+    public static void main(String[] args){
+        int i = 1, j = 5;
+         while (true){
+             System.out.println(i++);
+             if (i >= j)
+                 break;
+         }
+    }
+}
