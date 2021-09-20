ef74932c6580e504c3926775fb0628c2cf0758a0
00030 <jowl950313@gmail.com>
2021-09-20 16:48:49 +0900

    Static Block

---
+++ src/_0920_StaticBlock.java
@@ -0,0 +1,14 @@
+public class _0920_StaticBlock {
+    static int sumOneToTen;
+
+    static {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++)
+            sum += i;
+        sumOneToTen = sum;
+    }
+    public static void main(String[] args){
+        System.out.println(sumOneToTen);
+
+    }
+}
