90c4cf7f4240702f2e4fcef806917af693f6c1f1
00030 <jowl950313@gmail.com>
2021-09-23 22:24:34 +0900

    String 비교 equalsIgnoreCase

---
+++ src/_0923_String2.java
@@ -0,0 +1,21 @@
+public class _0923_String2 {
+    public static void main(String[] args){
+        String s1 = "Hi, Java!";
+        String s2 = new String("Hi Java!");
+        String s3 = "Hi, Code!";
+        String s4 = "Hi, java!";
+
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s3));
+        System.out.println(s1.equals(s4));
+        System.out.println(s1.equalsIgnoreCase(s4));
+
+        System.out.println(s1.compareTo(s3));
+        System.out.println(s1.compareToIgnoreCase(s4));
+        System.out.println(s3.compareTo(s4));
+        System.out.println("Hi, Java!".compareToIgnoreCase("hi, java!"));
+
+        System.out.printf("'J' - 'C' = %d\n", 'J' - 'C');
+        System.out.printf("'C' - 'j' = %d\n", 'C' - 'j');
+    }
+}
