0c9fd8a1a661c352520cdfaa8b6a72a94ff9d3c7
00030 <jowl950313@gmail.com>
2021-09-24 03:33:08 +0900

    String 클래스에서 제공하는 method

---
+++ src/_0924_StringMethod.java
@@ -0,0 +1,15 @@
+public class _0924_StringMethod {
+    public static void main(String[] args){
+        // char charAt(int index)       : index가 지정한 문자를 반환
+        // String concat(String s)      : 문자열 s를 현재 문자열 뒤에 붙임
+        // boolean contains(String s)   : 문자열 s를 포함하는지 조사
+        // boolean endsWith(String s)   : 끝나는 문자열이 s인지 조사
+        // boolean isEmpty()            : 문자열의 길이가 0이면 true 반환
+        // int length()                 : 문자열 길이 반환
+        // boolean startsWith(String s) : 시작하는 문자열이 s인지 조사
+        // String substring(int index)  : index부터 시작하는 문자열의 일부를 반환
+        // String toLowerCase()         : 문자열 전체를 소문자로 변환
+        // String toUpperCase()         : 문자열 전체를 대문자로 변환
+        // String trim()                : 문자열 앞뒤에 있는 공백 제거 후 반환
+    }
+}
